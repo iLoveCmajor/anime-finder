37a6a5abbe24c06de60c55bcaef124e6680e16e6
Vladimir Lopatkin <vlopatkinv@gmail.com>
2026-09-04 14:10:22 +0400

    minor fixes

--- evaluation_utils.py
+++ evaluation_utils.py
@@ -1,7 +1,6 @@
 import time
 
 from tqdm import tqdm
-from rag_helper import RAGBase
 
 
 def calc_price(usage):
@@ -65,48 +64,6 @@ def llm_structured_retry(
             if attempt == max_retries - 1:
                 raise
             time.sleep(2 ** attempt)
-
-
-class RAGWithUsage(RAGBase):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.usages = []
-        self.last_usage = None
-
-    def reset_usage(self):
-        self.usages = []
-        self.last_usage = None
-
-    def search(self, query, num_results=5):
-        boost_dict = {"question": 1.0, "answer": 2.0, "section": 0.1}
-        filter_dict = {"course": self.course}
-
-        return self.index.search(
-            query,
-            num_results=num_results,
-            boost_dict=boost_dict,
-            filter_dict=filter_dict
-        )
-
-    def llm(self, prompt):
-        input_messages = [
-            {"role": "developer", "content": self.instructions},
-            {"role": "user", "content": prompt}
-        ]
-
-        response = self.llm_client.responses.create(
-            model=self.model,
-            input=input_messages
-        )
-
-        self.last_usage = response.usage
-        self.usages.append(response.usage)
-
-        return response.output_text
-
-    def total_cost(self):
-        return calc_total_price(self.usages)
 
 
 def map_progress(pool, seq, f):
